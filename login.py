51ec44aae1438fe5e2d728083ceefa8eb50d8e41
agulujan41proj <agulujan41proj@gmail.com>
2024-03-28 22:52:45 -0300

    Conectamos la interfaz con python

---
+++ login.py
@@ -0,0 +1,22 @@
+from PyQt5 import QtCore,QtWidgets,uic
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtCore import Qt
+
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import * 
+import sys
+class Login(QtWidgets.QMainWindow):
+    def __init__(self, parent=None):
+        super(Login,self).__init__(parent)
+        self.setFixedSize(447,458)
+        uic.loadUi("ui/login.ui",self)
+
+
+
+
+
+
+app = QApplication(sys.argv)
+object = Login()
+object.show()
+app.exec_()
