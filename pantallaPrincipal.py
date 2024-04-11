5293bb29a59dfc01953ab3058d07971b0a69cb87
agulujan41proj <agulujan41proj@gmail.com>
2024-04-11 11:36:26 -0300

    translacion pantalla

--- pantallaPrincipal.py
+++ pantallaPrincipal.py
@@ -20,6 +20,22 @@ class PantallaPrincipal(QtWidgets.QMainWindow):
     
         self.labelTipoUsuario.setText(self.usuarios.obtenerTipoUsuario(self.datosUsuario[0][1])[0][0].upper())
         self.btnMenu.clicked.connect(self.deslizarMenu)
+        self.btnAdministracion.clicked.connect(self.administracion)
+        self.btnEmpleados.clicked.connect(self.empleados)
+        self.btnTurnos.clicked.connect(self.turnos)
+        self.btnCalendario.clicked.connect(self.calendario)
+        self.pantallas = []
+        self.pantallas.append(self.frameAdministracion)
+        self.pantallas.append(self.frameTurnos)
+        self.pantallas.append(self.frameCalendario)
+        self.pantallas.append(self.frameBienvenida)
+        self.pantallas.append(self.frameEmpleados)
+
+        self.bienvenida()
+        
+        if idUsuario == 3:
+            self.btnAdministracion.hide()
+
     def deslizarMenu(self):
         anchoMenu = self.frameMenu.width()
         nuevoAncho = 0
@@ -31,7 +47,29 @@ class PantallaPrincipal(QtWidgets.QMainWindow):
         self.animation.setEndValue(nuevoAncho)
         self.animation.setEasingCurve(QtCore.QEasingCurve.InOutQuart)
         self.animation.start()
+    def bienvenida(self):
+        self.labelTituloPantalla.setText("Inicio")
+        self.ocultarPantallasMenos(self.frameBienvenida)
+    
+    def administracion(self):
+        self.labelTituloPantalla.setText("Administracion")
+        self.ocultarPantallasMenos(self.frameAdministracion)
+    def empleados(self):
+        self.labelTituloPantalla.setText("Empleados")
+        self.ocultarPantallasMenos(self.frameEmpleados)
+    def turnos(self):
+        self.labelTituloPantalla.setText("Turnos")
+        self.ocultarPantallasMenos(self.frameTurnos)
+
+    def calendario(self):
+        self.labelTituloPantalla.setText("Calendario")
+        self.ocultarPantallasMenos(self.frameCalendario)
+    def ocultarPantallasMenos(self,pantallaAMostrar):
+        for pantalla in self.pantallas:
+            pantalla.hide()
+        pantallaAMostrar.show()
+
 app = QApplication(sys.argv)
-object = PantallaPrincipal(1)
+object = PantallaPrincipal(3)
 object.show()
 app.exec_()
